03242fbb9ed0d7d583ef1e13ab22708227bc4331
unknown <prudhvisetti007@gmail.com>
2022-05-12 16:42:04 +0530

    added headers

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Company } from '../model/company.model';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor(private http: HttpClient) { }
-  baseUrl: string = 'http://localhost:8010/api/v1.0/market/company';
 
+  constructor(private http: HttpClient) { }
+
+  baseUrl: string = 'http://localhost:5000/api/v1.0/market/company';
+  getHeaders() {
+    let headers = new HttpHeaders();
+    headers = headers.append('content-type', 'application/json')
+    headers = headers.append('Access-Control-Allow-Origin', '*')
+    headers = headers.append('content-type', 'application/x-www-form-urlencoded')
+    return headers;
+  }
   //get all companies
   getCompanies() {
-    return this.http.get<Company[]>(this.baseUrl + '/getall')
+    return this.http.get<Company[]>(this.baseUrl + '/getall', { 'headers': this.getHeaders() })
   }
   //get company by id
   getCompanyById(id: number) {
-    return this.http.get<Company>(this.baseUrl + '/info/' + id)
+    return this.http.get<Company>(this.baseUrl + '/info/' + id, { 'headers': this.getHeaders() })
   }
   //add company
   createCompany(company: Company) {
-    return this.http.post(this.baseUrl + '/register', company);
+    return this.http.post(this.baseUrl + '/register', company, { 'headers': this.getHeaders() });
   }
   //delete company
   deleteCompany(id: number) {
-    return this.http.delete(this.baseUrl + '/delete/' + id)
+    return this.http.delete(this.baseUrl + '/delete/' + id, { 'headers': this.getHeaders() })
   }
 
 
--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Stock } from '../model/stock.model';
 import { StockPrices } from '../model/stockPrices.model';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,20 @@ export class StockService {
   constructor(private http: HttpClient) { }
   baseUrl: string = 'http://localhost:8011/api/v1.0/market/stock';
 
-
+  getHeaders() {
+    let headers = new HttpHeaders();
+    headers = headers.append('content-type', 'application/json')
+    headers = headers.append('Access-Control-Allow-Origin', '*')
+    headers = headers.append('content-type', 'application/x-www-form-urlencoded')
+    return headers;
+  }
   //add stock
   addStock(id: number, stock: Stock) {
-    return this.http.post(this.baseUrl + '/add/' + id, stock);
+    return this.http.post(this.baseUrl + '/add/' + id, stock, { 'headers': this.getHeaders() });
   }
   //get stockPrices by id
   getCompanyById(id: number, startDate: String, endDate: String) {
-    return this.http.get<StockPrices[]>(this.baseUrl + '/get/' + id + '/' + startDate + '/' + endDate)
+    return this.http.get<StockPrices[]>(this.baseUrl + '/get/' + id + '/' + startDate + '/' + endDate, { 'headers': this.getHeaders() })
   }
 
 
